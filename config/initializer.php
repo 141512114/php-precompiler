46d8a2c17a8fe27b7618ca117d257aa0d03338e0
Nolan Bjarne <derjuniorschiemann@gmail.com>
2025-04-08 18:08:51 +0200

    Revert "Add initial project setup with Composer and IntelliJ configuration"
    This reverts commit 60e111c99a83a41ab58703a38db304dbf3412a47.

--- config/initializer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-ob_start();
-
-define('TEST', "Hello World out of a define");
-const TEST2 = "Hello World out of a constant";
-$_TEST = "Hello World out of a normal variable";
