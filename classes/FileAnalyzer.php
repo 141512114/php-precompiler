080bd031b4fad131ca5dfe9a5c9fb95e1246afb2
Nolan Bjarne <nolan.bjarne@icloud.com>
2026-02-14 18:22:15 +0100

    Add migration state XML files and update FileAnalyzer for improved code style

--- classes/FileAnalyzer.php
+++ classes/FileAnalyzer.php
@@ -11,7 +11,7 @@ class FileAnalyzer
      */
     public function getCurrentFile(): array|string
     {
-        return pathinfo( $_SERVER[ 'PHP_SELF' ], PATHINFO_BASENAME );
+        return pathinfo($_SERVER['PHP_SELF'], PATHINFO_BASENAME);
     }
 
     /**
@@ -21,11 +21,11 @@ class FileAnalyzer
      *
      * @return string[] An array of include/require paths found in the file.
      */
-    public function findIncludes( string $file ): array
+    public function findIncludes(string $file): array
     {
-        $includes     = [];
+        $includes = [];
         // @TODO: Instead of looking for includes in the current file, it might be more efficient to use `get_included_files()` to get all included files
-        $fileContents = file_get_contents( $file );
+        $fileContents = file_get_contents($file);
         // @TODO: Then it is possible to combine the files with the current file and remove their implementations from the code (anywhere), so that the current file only contains the includes and no other code
 
         if ( $fileContents === FALSE ) {
@@ -34,12 +34,12 @@ class FileAnalyzer
 
         // Use regex to find all include/require statements
         // @TODO: Consider using a more robust parser for complex cases => this regex is basic and may not cover all edge cases.
-        preg_match_all( '/\b(?<!\$)(?:include|require)(_once)?\s*\(?\s*(.+?)\s*\)?\s*;/', $fileContents, $matches );
+        preg_match_all('/\b(?<!\$)(?:include|require)(_once)?\s*\(?\s*(.+?)\s*\)?\s*;/', $fileContents, $matches);
 
-        if ( !empty( $matches[ 2 ] ) ) {
-            foreach ( $matches[ 2 ] as $include ) {
-                $includes[] = trim( $include );
-            }
+        foreach ( $matches[2] as $include ) {
+            $trimmedInclude = trim($include);
+            if ( empty($trimmedInclude) ) continue;
+            $includes[] = $trimmedInclude;
         }
 
         return $includes;
